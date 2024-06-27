20637ab9d0f46fcfea54421b4d5b799764c2c307
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-27 16:18:44 -0700

    Commit

--- MyCustomFrameworkProject/WebViewViewController.swift
+++ MyCustomFrameworkProject/WebViewViewController.swift
@@ -6,24 +6,18 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://www.apple.com/in/") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
