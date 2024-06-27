635125a359bb7a4f2fb06580789ee8fbb5395b90
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-27 12:49:55 -0700

    Commit

---
+++ MyCustomFrameworkProject/MyCustomFramework.framework/Headers/MyCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyCustomFramework.h
+//  MyCustomFramework
+//
+//  Created by Arpit iOS Dev. on 27/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCustomFramework.
+FOUNDATION_EXPORT double MyCustomFrameworkVersionNumber;
+
+//! Project version string for MyCustomFramework.
+FOUNDATION_EXPORT const unsigned char MyCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCustomFramework/PublicHeader.h>
+
+
---
+++ MyCustomFrameworkProject/PhoneCallViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneCallViewController.swift
+//  MyCustomFrameworkProject
+//
+//  Created by Arpit iOS Dev. on 27/06/24.
+//
+
+import UIKit
+
+class PhoneCallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
